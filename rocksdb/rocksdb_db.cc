1fa52f299357ccc5f6b177d1cb90b7e45b787e5a
gs201 <sg20180546@gmail.com>
2023-12-07 20:26:37 +0900

    workloadg

--- rocksdb/rocksdb_db.cc
+++ rocksdb/rocksdb_db.cc
@@ -198,14 +198,14 @@ void RocksdbDB::Init() {
   std::vector<rocksdb::ColumnFamilyHandle *> cf_handles;
   printf("YCSB GetOptions==================\n");
   GetOptions(props, &opt, &cf_descs);
-  printf("YCSB GetOptions==================\n");
+  printf("YCSB GetOptions================== %p\n",opt.env->GetFileSystem().get());
 #ifdef USE_MERGEUPDATE
   opt.merge_operator.reset(new YCSBUpdateMerge);
 #endif
 
   rocksdb::Status s;
   if (props.GetProperty(PROP_DESTROY, PROP_DESTROY_DEFAULT) == "true") {
-    s = rocksdb::DestroyDB(db_path, opt);
+    // s = rocksdb::DestroyDB(db_path, opt);
     if (!s.ok()) {
       throw utils::Exception(std::string("RocksDB DestroyDB: ") + s.ToString());
     }
