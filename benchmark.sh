7ea11f4262a8358c7ce2b91ea35c49af20d05451
gs201 <sg20180546@gmail.com>
2023-12-20 22:40:30 +0900

    script

--- benchmark.sh
+++ benchmark.sh
@@ -6,8 +6,8 @@ do
         sudo mkdir -p ~/log
         echo "mq-deadline" | sudo tee /sys/block/nvme0n1/queue/scheduler
         sudo /home/femu/CAZA/rocksdb/plugin/zenfs/util/zenfs mkfs --force --enable_gc   --zbd=/nvme0n1 --aux_path=/home/femu/log
-        sudo cp ~/YCSB-cpp/rocksdb/zenfsoptions.ini ~/log/zenfsoptions.ini
-        sudo ~/YCSB-cpp/ycsb -load -run -db rocksdb -P workloads/workload${workload_type} -P \
+        sudo cp /home/femu/YCSB-cpp/rocksdb/zenfsoptions.ini ~/log/zenfsoptions.ini
+        sudo /home/femu/YCSB-cpp/ycsb -load -run -db rocksdb -P workloads/workload${workload_type} -P \
                  rocksdb/rocksdb.properties -s > /home/femu/access_testdata/YCSB/CAZACA_${workload_type}_i
     done
 done
