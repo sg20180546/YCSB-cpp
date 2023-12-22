e003ee59089c5a1ea0943546892455281d0bf8cb
gs201 <sg20180546@gmail.com>
2023-12-22 14:46:27 +0900

    script

--- benchmark.sh
+++ benchmark.sh
@@ -8,7 +8,7 @@ for i in 1 2 3
 do
     for workload_type in a d e
     do  
-        for SCHEME in $LIZA $LIZACA $CAZA $CAZACA
+        for SCHEME in $LIZA $CAZA $CAZACA $LIZACA
         do
                 if [ $SCHEME -eq $LIZA ]; then
                     RESULT_PATH=${RESULT_DIR_PATH}/A_${workload_type}_LIZA_${i}.txt
