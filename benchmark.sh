1a5f0adf7e5b148c9a6cb399c97b6d402da6c483
gs201 <sg20180546@gmail.com>
2024-02-23 17:11:09 +0900

    pcache4gb

--- benchmark.sh
+++ benchmark.sh
@@ -6,6 +6,8 @@ PCA=2
 
 OPTIONS=/home/sungjin/YCSB-cpp/rocksdb/zenfsoptions.ini
 RESULT_DIR_PATH=/home/sungjin/access_testdata/YCSB
+
+CACHESIZE=4
 for i in 1 2 3
 do
     for workload_type in a
@@ -13,13 +15,13 @@ do
         for SCHEME in  $PCA
         do
                 if [ $SCHEME -eq $BASELINE ]; then
-                    RESULT_PATH=${RESULT_DIR_PATH}/BASELINE_${workload_type}_${i}.txt
+                    RESULT_PATH=${RESULT_DIR_PATH}/BASELINE_${workload_type}_${4}GB_${i}.txt
                     OPTIONS=/home/sungjin/YCSB-cpp/rocksdb/smr_baseline.ini
                 elif [ $SCHEME -eq $SMR_ZC ]; then
-                    RESULT_PATH=${RESULT_DIR_PATH}/SMR_ZC_${workload_type}_${i}.txt
+                    RESULT_PATH=${RESULT_DIR_PATH}/SMR_ZC_${workload_type}_${4}GB_${i}.txt
                     OPTIONS=/home/sungjin/YCSB-cpp/rocksdb/smr_large_io.ini
                 elif [ $SCHEME -eq $PCA ]; then
-                    RESULT_PATH=${RESULT_DIR_PATH}/SMR_ZC_pca_${workload_type}_${i}.txt
+                    RESULT_PATH=${RESULT_DIR_PATH}/SMR_ZC_pca_${workload_type}_${4}GB_${i}.txt
                     OPTIONS=/home/sungjin/YCSB-cpp/rocksdb/smr_pca.ini
                 else  
                     echo "error"
