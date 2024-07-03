f7f436b933cbe7d9caa0ed8fab8b7f054dfdd93f
micron <sg20180546@gmail.com>
2024-07-03 19:53:08 +0900

    script_femu

---
+++ benchmark_fast.sh
@@ -0,0 +1,75 @@
+
+
+BASELINE=0
+ZEUFS=1
+
+OPTIONS=/home/sungjin/YCSB-cpp/rocksdb/zenfsoptions.ini
+RESULT_DIR_PATH=/home/sungjin/FAST_testdata/YCSB
+
+CACHESIZE=4
+
+# PHASE=load
+
+A="a"
+SCANWRITERANDOM="scanwriterandom"
+
+for i in 1 2
+do
+    for WORKLOAD_TYPE in zipfian latest uniform
+    do  
+        for SCHEME in $BASELINE $ZEUFS
+        do
+                if [ $SCHEME -eq $BASELINE ]; then
+                    RESULT_PATH=${RESULT_DIR_PATH}/${WORKLOAD_TYPE}_LSE.txt
+                    OPTIONS=/home/sungjin/YCSB-cpp/rocksdb/FAST_baseline.ini 
+                elif [ $SCHEME -eq $ZEUFS ]; then
+                    RESULT_PATH=${RESULT_DIR_PATH}/${WORKLOAD_TYPE}_ZEUFS.txt
+                    OPTIONS=/home/sungjin/YCSB-cpp/rocksdb/FAST_motiv_zonereset.ini
+                else  
+                    echo "error"
+                fi
+            if [ -f ${RESULT_PATH} ]; then
+                echo "already $RESULT_PATH exists"
+                # sleep 30
+                sleep 5
+                continue
+                # break
+            fi
+            # if [ "$WORKLOAD_TYPE" = "$A" ]; then
+            #     PHASE=load
+            # else
+            #     PHASE=run
+            # fi
+
+            while : 
+                do
+                /home/femu/zone_reset_all 0 1300
+                sudo rm -rf /home/sungjin/log
+                sudo mkdir -p /home/sungjin/log
+                echo "mq-deadline" | sudo tee /sys/block/nvme0n1/queue/scheduler
+                sudo /home/sungjin/CAZAandZACA/rocksdb/plugin/zenfs/util/zenfs mkfs --force --enable_gc   --zbd=/nvme0n1 --aux_path=/home/sungjin/log > mkfs_log
+
+                echo ${RESULT_PATH}
+                sudo cp ${OPTIONS} /home/sungjin/log/zenfsoptions.ini
+
+                sudo /home/sungjin/YCSB-cpp/ycsb -run -db rocksdb -P workloads/workload_${WORKLOAD_TYPE} -P \
+                        rocksdb/rocksdb.properties -s > ${RESULT_DIR_PATH}/tmp
+                
+                if grep -q "samezone score" ${RESULT_DIR_PATH}/tmp; then
+                    cat ${RESULT_DIR_PATH}/tmp > ${RESULT_PATH}
+                    rm -rf ${RESULT_DIR_PATH}/tmp
+                    break
+                else
+                    cat ${RESULT_DIR_PATH}/tmp > ${RESULT_DIR_PATH}/failed
+                    sleep 5
+                    # break
+                fi
+            done
+        done
+    done
+done
+
+echo "all done"
+
+sudo /home/sungjin/access_testdata/sendresultmail
+
