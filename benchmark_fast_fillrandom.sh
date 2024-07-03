cdf5f2deb421e89084a7fb6592344efe300fcfd3
gs201 <sg20180546@gmail.com>
2024-07-04 00:43:39 +0900

    fillrandomscript

--- benchmark_fast_fillrandom.sh
+++ benchmark_fast_fillrandom.sh
@@ -43,7 +43,7 @@ do
 
             while : 
                 do
-                /home/femu/zone_reset_all 0 100
+                /home/femu/zone_reset_all 0 25
                 sudo rm -rf /home/femu/log
                 sudo mkdir -p /home/femu/log
                 echo "mq-deadline" | sudo tee /sys/block/nvme0n1/queue/scheduler
